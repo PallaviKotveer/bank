84905bee4ae20ff7270d4087083809048ae2ea4c
PallaviKotveer <pallavikotvir@gmail.com>
2020-06-21 02:31:05 -0400

    test case complete for currency

--- test/bankingApp/CurrencyTest.java
+++ test/bankingApp/CurrencyTest.java
@@ -15,7 +15,7 @@ public class CurrencyTest {
 	 * 	GBP = Great British Pounds
 	 * 	HKD = Hong Kong Dollars
 	 */
-	public Currency CAD, EUR, GBP, HKD;
+	public Currency CAD, EUR, GBP, HKD, INR;
 	
 	@Before
 	public void setUp() throws Exception {
@@ -23,6 +23,7 @@ public class CurrencyTest {
 		CAD = new Currency("CAD", 0.75);
 		EUR = new Currency("EUR", 1.14);
 		HKD = new Currency("HKD", 0.13);
+		INR = new Currency("INR", 0.013);
 	}
 
 	@Test
@@ -53,7 +54,18 @@ public class CurrencyTest {
 		// 2. Change the rate
 		// 3. Assert that the newRate is correct
 		// You will end up with 2 assert() statements in this function.
-//		fail("Write test case here");
+		assertEquals(0.75, CAD.getRate(),0);
+		System.out.println(CAD.getRate());
+		CAD.setRate(0.73);
+		assertEquals(0.73, CAD.getRate(),0);
+		CAD.setRate(0.75);
+		
+		
+		assertEquals(1.14, EUR.getRate(),0);
+		System.out.println(EUR.getRate());
+		EUR.setRate(0.73);
+		assertEquals(0.73, EUR.getRate(),0);
+		EUR.setRate(1.14);
 	}
 	
 	@Test
@@ -62,6 +74,7 @@ public class CurrencyTest {
 		assertEquals(7.5, CAD.valueInUSD(10),0);
 		assertEquals(11.4, EUR.valueInUSD(10),0);
 		assertEquals(1.3, HKD.valueInUSD(10),0);
+//		assertEquals(1.3, INR.valueInUSD(10),0);
 	}
 	
 	@Test
@@ -73,6 +86,15 @@ public class CurrencyTest {
 		// 3. Assert that the value in "this" currency is correct
 		// You will end up with 2 assert() statements in this function.
 //		fail("Write test case here");
+//		asserEquals(23,CAD.)
+		assertEquals(0.75, CAD.valueInUSD(1),0);
+		System.out.println(EUR.valueInUSD(10));
+		assertEquals(6.58, EUR.valueInThisCurrency(10, CAD),0);
+		
+		assertEquals(0.75, CAD.valueInUSD(1),0);
+		System.out.println(HKD.valueInUSD(10));
+		assertEquals(57.69, HKD.valueInThisCurrency(10, CAD),0);
+		
 		
 	}
 
